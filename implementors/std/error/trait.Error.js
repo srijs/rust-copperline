4b156e6684112c39376e56d885a5b3663ca20105
Sam Rijs <srijs@airpost.net>
2015-09-20 17:15:49 +1000

    update docs for 0.2.1

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['copperline'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='copperline/enum.Error.html' title='copperline::Error'>Error</a>",];
+implementors['copperline'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='copperline/enum.Error.html' title='copperline::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
